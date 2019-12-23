a5b0d2a87cd950eee78e645f958c9edddf7097b7
Nikita <45326590+Nebe1Pace@users.noreply.github.com>
2019-12-23 14:33:16 +0500

    comment

--- Assets/Scripts/Move_II.cs
+++ Assets/Scripts/Move_II.cs
@@ -6,9 +6,9 @@ public class Move_II : MonoBehaviour
 {
     Rigidbody Rbody1, Rbody2;
     public Rigidbody DrugoiRbody1, DrugoiRbody2;
-    public Vector3 r1, r2;
-    public Vector3 R1, R2;
-    public Vector3 V1, V2;
+    public Vector3 r1, r2; //тел
+    public Vector3 R1, R2; //силы
+    public Vector3 V1, V2; 
     float g = 6.67408e-2F;
     public float m, M;
     void Start()
@@ -16,27 +16,23 @@ public class Move_II : MonoBehaviour
         Rbody1 = GetComponent<Rigidbody>();
         Rbody1.position = r1;
         Rbody1.velocity = V1;
-        Debug.Log(Rbody1.position);
 
         Rbody2 = GetComponent<Rigidbody>();
         Rbody2.position = r2;
         Rbody2.velocity = V2;
-        Debug.Log(Rbody2.position);
     }
     void Update()
     {
         r1 = Rbody1.position - DrugoiRbody1.position;
         R1 = Rbody1.position;
-        float l1 = -g * m / Mathf.Pow(r1.magnitude, 3);
-        float L1 = -g * M / Mathf.Pow(R1.magnitude, 3);
-        Rbody1.AddForce(l1 * r1 + L1 * R1);
-        Debug.Log(l1 * r1);
+        float l1 = -g * m / Mathf.Pow(r1.magnitude, 2);
+        float L1 = -g * M / Mathf.Pow(R1.magnitude, 2);
+        Rbody1.AddForce(l1 * r1 + L1 * R1); //Добавляет крутящий момент к физическому телу
 
         r2 = Rbody2.position - DrugoiRbody2.position;
         R2 = Rbody2.position;
-        float l2 = -g * m / Mathf.Pow(r2.magnitude, 3);
-        float L2 = -g * M / Mathf.Pow(R2.magnitude, 3);
-        Rbody2.AddForce(l2 * r2 + L2 * R2);
-        Debug.Log(l2 * r1);
+        float l2 = -g * m / Mathf.Pow(r2.magnitude, 2);
+        float L2 = -g * M / Mathf.Pow(R2.magnitude, 2);
+        Rbody2.AddForce(l2 * r2 + L2 * R2); //Добавляет крутящий момент к физическому телу
     }
 }
